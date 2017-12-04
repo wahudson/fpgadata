940b2c9a9223faa677d785694573c27964a8b16b
William Hudson <William.Hudson542@gmail.com>
2017-12-03 19:39:33 -0700

    Error:  Revise class to be a little more C++ like.     New msg()  returns an ostream reference so multiple outputs can be appended.     New Init_ostream()  can substitute ostringstream for testing.     New has_err()  to test error flag.     The old methods are obsolete, but still in use.

--- trunk/src/Error.cpp
+++ trunk/src/Error.cpp
@@ -1,15 +1,75 @@
-// Author:  William A. Hudson  2011-11-28
+// Author:  William A. Hudson  2017-12-01
 //
 // Error class.
+//     Provide a global error message stream and flag.
 //--------------------------------------------------------------------------
 
 #include <iostream>
 #include "Error.h"
 
-using namespace std;
 
 // Init static Class Data:
-int Error::Error_cnt = 0;
+int		Error::Error_cnt = 0;
+std::ostream*	Error::cErr      = &std::cerr;
+
+
+/*
+* Init error stream.  (class method)
+*    Optional - default is cerr.
+*    This allows substituting an ostringstream for testing.
+* call:
+*    Error::Init_ostream( &cerr )
+*/
+void
+Error::Init_ostream( std::ostream* erx )
+{
+    cErr = erx;
+}
+
+
+/*
+* Get error flag.  (class method)
+*    Return true if error messages have been sent.
+* call:
+*    Error::has_err()
+*/
+int
+Error::has_err()
+{
+    return  Error_cnt;
+}
+
+
+/*
+* Send error message.  (class method)
+*    Returns an error stream with initial message "Error:  ".
+*    This allows appending further output and using io manipulators.
+*    User is rsponsible for the terminating newline.
+*    Option to pass cstring text as an argument.
+* call:
+*    Error::msg() << "text" << .. <<endl;
+*    Error::msg( "text\n" );
+*    Error::msg( "text" ) << .. <<endl;
+*/
+std::ostream&
+Error::msg()
+{
+    Error_cnt++;
+    *cErr << "Error:  ";
+    return  *cErr;
+}
+
+std::ostream&
+Error::msg( const char* text )
+{
+    Error::msg() << text;
+    return  *cErr;
+}
+
+
+//#!! Obsolete methods:
+
+using namespace std;
 
 // call:
 //    Error::err()		Get global error count.
@@ -48,15 +108,5 @@ Error::err( const char* m1,  const char* m2,  const char* m3,  const char* m4 )
 {
     Error_cnt++;
     cerr << "Error:  " << m1 << m2 << m3 << m4 << endl;
-}
-
-
-// Instance Methods:
-int
-Error::msg( char* m1 )
-{
-    cerr << "Error msg:  " << m1 << endl;
-    count = 1;
-    return count;
 }
 
--- trunk/src/Error.h
+++ trunk/src/Error.h
@@ -2,15 +2,21 @@
 #ifndef Error_P
 #define Error_P
 
-
 //--------------------------------------------------------------------------
 // Error class
 //--------------------------------------------------------------------------
 
 class Error {
   private:
-    static int		Error_cnt;
-    int			count;
+    static int			Error_cnt;
+    static std::ostream		*cErr;
+
+  public:	// Class Methods
+    static void			Init_ostream( std::ostream* erx );
+    static int			has_err();
+    static std::ostream&	msg();
+    static std::ostream&	msg( const char* text );
+
   public:
     static int		err();
     static void		err( const char* );
@@ -18,8 +24,8 @@ class Error {
     static void		err( const char*,  const char*,  const char* );
     static void		err( const char*,  const char*,  const char*,  const char* );
 
-    int			msg( char* );
 };
 
 
 #endif
+
